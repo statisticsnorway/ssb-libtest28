00f87994cdff28bd7c661348f307837b74b439e2
Ola Vik Presterud <126689460+olaprest-ssb@users.noreply.github.com>
2023-12-12 10:33:43 +0100

    Initial commit

---
+++ src/ssb_libtest28/__main__.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """SSB Libtest28."""
+
+
+if __name__ == "__main__":
+    main(prog_name="ssb-libtest28")  # pragma: no cover
